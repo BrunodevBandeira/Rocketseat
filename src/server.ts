5b5ac1d633e6a535ba4d022cdc32503ee46af0a4
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-25 23:23:34 -0300

    Adicionando os métodos GET e PUT

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,29 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app =  express();
+
+
+//request = é tudo aquilo que estamos recebendo.
+//responde = é tudo aquilo que estamos enviando.
+/*
+   GET = BUSCA
+   POST = SALVA
+   PUT = ALTERA
+   DELETE = APAGAR
+   PATCH = ALTERAÇÃO ESPECIFICA
+*/
+
+
+// Entenda o GET como a rota(Recurso API)
+app.get("/users", (request, response) => {
+    //return response.send("Hellow word - NLW04");
+    return response.json({ message: "Hello World - NLW04"});
+});
+
+// Entenda o POST como request, response 
+
+app.post("/", ( request, response) => {
+    return response.json({ message: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(3310, () => console.log("Server is running"));
